440e72b1d8743129c6d09405ffa27483ec1b1dbe
Anar Asgarli <esgerlianar003@gmail.com>
2025-01-27 23:34:31 +0400

    Chore: createJSONStorage added

--- store/persist.js
+++ store/persist.js
@@ -1,5 +1,5 @@
 import { create } from "zustand";
-import { persist } from "zustand/middleware";
+import { createJSONStorage, persist } from "zustand/middleware";
 
 const usePersistStore = create(
   persist(
@@ -10,7 +10,7 @@ const usePersistStore = create(
     }),
     {
       name: "persist-data",
-      getStorage: () => localStorage,
+      storage: createJSONStorage(() => localStorage),
     }
   )
 );
